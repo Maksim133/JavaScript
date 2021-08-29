fc24325edb331462001a75329c6c9c87af4fa713
Maksim133 <pravo24kr2008@gmail.com>
2021-08-29 16:51:28 +0700

    Изучали циклы for, while. Разбирали алгоритмы суммирования чисел, переворачивания строк.

---
+++ JS/lessons/lesson01.js
@@ -0,0 +1,82 @@
+// const sumOfSquares = () =>
+// {   let result = 0;
+//     for (let i = 1; i<=10; i++) {
+     
+//         result += i**2;
+//     }
+//     return result;
+// };
+
+// console.log (sumOfSquares());
+
+
+// const sumOfSquares = () =>{
+    
+//     let result = 0;
+//     let inter = 1;
+
+//     while (inter <= 10) {
+//         result += inter**2;
+//         inter+=1;
+//     }
+   
+//     return result;
+// };
+
+// console.log (sumOfSquares());
+
+// const reverseNumber = (n)=>{
+//     if(n<0){
+//     l=-n
+//     let c = String(l);
+//     let i=0;
+//     let b =c.length-1;
+//     let array = '';
+
+//     while(b>=i){
+//     array+= c[b];
+//     b--;
+//     }
+
+//     return(-array);
+
+//     }
+
+//     if(n>0){
+//     let c = String(n);
+//     let i=0;
+//     let b =c.length-1;
+//     let array = '';
+
+//     while(b>=i){
+//     array+= c[b];
+//     b--;
+//     }
+//     return(array);
+// }
+// }
+// console.log(reverseNumber(3124));
+
+// let a = null; //переменная с пустотой
+// console.log(a); // специальное значение undefined
+// typeof (a); //undefined 
+
+const SumNumbers = (n)=>{
+    
+    let str = String(n);
+    let result = 0;
+    let i = str.length;
+
+    while (i != 0 && numbers.length != 1) {
+        number1 = null;
+        number1 = str[i];
+        number1 = Number(number);
+        result+= number;
+        numbers = String(result);
+        i-=1;
+        
+        
+    }
+
+}
+console.log(SumNumbers(3124));
